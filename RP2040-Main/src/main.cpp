9051bf1516fc44126c6ef242d78826d30ae0cfcb
fTomo-robot <96418441+fTomo-robot@users.noreply.github.com>
2024-01-05 11:33:51 +0900

    okei

---
+++ RP2040-Main/src/main.cpp
@@ -0,0 +1,426 @@
+#include <Arduino.h>
+#include <SCServo.h>
+#include <HardwareSerial.h>
+
+SMS_STS st;
+
+byte ID[2];
+s16 Position[2];
+u16 Speed[2];
+byte ACC[2];
+
+// SerialUSB  Serial;
+// SerialUART Serial2;
+
+void setup()
+{
+  Serial2.begin(1000000);
+  Serial2.setTX(8);
+  Serial2.setRX(9);
+  st.pSerial = &Serial2;
+  delay(1000);
+  ID[0] = 1;
+  ID[1] = 2;
+  Speed[0] = 3400;
+  Speed[1] = 3400;
+  ACC[0] = 50;
+  ACC[1] = 50;
+}
+
+void loop()
+{
+  Position[0] = 4095;
+  Position[1] = 4095;
+  st.SyncWritePosEx(ID, 2, Position, Speed, ACC);//舵机(ID1/ID2)以最高速度V=3400步/秒，加速度A=50(50*100步/秒^2)，运行至P1=4095位置
+  delay(1884);//((P1-P0)/V)*1000+(V/(A*100))*1000
+
+  Position[0] = 0;
+  Position[1] = 0;
+  st.SyncWritePosEx(ID, 2, Position, Speed, ACC);//舵机(ID1/ID2)以最高速度V=3400步/秒，加速度A=50(50*100步/秒^2)，运行至P0=0位置
+  delay(1884);//((P1-P0)/V)*1000+(V/(A*100))*1000
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <Adafruit_NeoPixel.h>
+#define PIN        7
+#define NUMPIXELS 16
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int val = 0;
+int val1 = 0;
+int val2 = 0;// 読み取った値を格納する変数
+
+int MUX_A = A0;
+int MUX_B = A1;
+
+const int digitalPin[3] = {23, 24, 25};  // 任意のピン番号
+
+void digitalOutBinary(int value) {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(digitalPin[i], value & 1);
+    value >>= 1;
+  }
+}
+
+  digitalOutBinary:A0/A1
+  0:floor0/Red0
+  1:floor1/Green0
+  2:floor2/Bule0
+  3:floor3/Red1
+  4:PSD0/Green1
+  5:PSD1/Bule1
+  6:PSD2/LOAD_L
+  7:PSD3/LOAD_R
+
+
+
+
+#define a 0.8   //0<a<1の範囲　大きいほど効果大
+
+const int M0_A = 10;
+const int M0_B = 11;
+const int M1_A = 12;
+const int M1_B = 13;
+
+const int M2_A = 17;
+const int M2_B = 16;
+const int M3_A = 19;
+const int M3_B = 18;
+
+void forward() {
+  analogWrite( M0_A, 0 );
+  analogWrite( M0_B, 40 );
+  analogWrite( M1_A, 0 );
+  analogWrite( M1_B, 40 );
+  analogWrite( M2_A, 0 );
+  analogWrite( M2_B, 40 );
+  analogWrite( M3_A, 0 );
+  analogWrite( M3_B, 40 );
+}
+
+void Stop() {
+  analogWrite( M0_A, 0 );
+  analogWrite( M0_B, 0 );
+  analogWrite( M1_A, 0 );
+  analogWrite( M1_B, 0 );
+  analogWrite( M2_A, 0 );
+  analogWrite( M2_B, 0 );
+  analogWrite( M3_A, 0 );
+  analogWrite( M3_B, 0 );
+}
+
+void Left() {
+  analogWrite( M0_A, 0 );
+  analogWrite( M0_B, 70 );
+  analogWrite( M1_A, 0 );
+  analogWrite( M1_B, 70 );
+  analogWrite( M2_A, 70 );
+  analogWrite( M2_B, 0 );
+  analogWrite( M3_A, 0 );
+  analogWrite( M3_B, 70 );
+}
+
+void back() {
+  analogWrite( M0_A, 40 );
+  analogWrite( M0_B, 0 );
+  analogWrite( M1_A, 40 );
+  analogWrite( M1_B, 0 );
+  analogWrite( M2_A, 40 );
+  analogWrite( M2_B, 0 );
+  analogWrite( M3_A, 40 );
+  analogWrite( M3_B, 0 );
+}
+
+
+
+void setup() {
+  pixels.begin();
+
+  pinMode(23, OUTPUT);
+  pinMode(24, OUTPUT);
+  pinMode(25, OUTPUT);
+
+  pinMode(MUX_A, INPUT);
+  pinMode(MUX_B, INPUT);
+
+  digitalWrite(23, LOW);
+  digitalWrite(24, LOW);
+  digitalWrite(25, LOW);
+
+  Serial.begin(9600);
+}
+
+
+
+
+void loop() {
+
+  digitalOutBinary(0);
+  val1 = analogRead(A1);
+  digitalOutBinary(3);
+  val2 = analogRead(A1);
+  float Rval = ((val1 + val2) / 2 ) * 1.5;
+  float Rrc = 0;
+  Rrc = a * Rrc + (1 - a) * (float)Rval;
+  Serial.print('R');
+  Serial.println(Rrc);
+
+  digitalOutBinary(1);
+  val1 = analogRead(A1);
+  digitalOutBinary(4);
+  val2 = analogRead(A1);
+  float Gval = ( (val1 + val2) / 2) * 1.1;
+  float Grc = 0;
+  Grc = a * Grc + (1 - a) * (float)Gval;
+  Serial.print('G');
+  Serial.println(Grc);
+
+  digitalOutBinary(2);
+  val1 = analogRead(A1);
+  digitalOutBinary(5);
+  val2 = analogRead(A1);
+  float Bval = ((val1 + val2) / 2);
+  float Brc = 0;
+  Brc = a * Brc + (1 - a) * (float)Bval;
+  Serial.print('B');
+  Serial.println(Brc);
+
+  //delay(1000);
+  
+    if (Rrc > Grc) {
+     if (Rrc > Brc) {
+       Serial.println("RED");
+       Stop();
+
+       {
+         int i = 0;
+         pixels.clear();
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+           pixels.show();
+           delay(100);
+           i++;
+         }
+         delay(1000);
+       }
+       {
+         int i = 0;
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+           pixels.show();
+           delay(10);
+           i++;
+         }
+         delay(500);
+       }
+       {
+         int i = 0;
+         pixels.clear();
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+           pixels.show();
+           delay(100);
+           i++;
+         }
+         delay(1000);
+       }
+       {
+         int i = 0;
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+           pixels.show();
+           delay(10);
+           i++;
+         }
+         delay(500);
+       }
+       {
+         int i = 0;
+         pixels.clear();
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+           pixels.show();
+           delay(100);
+           i++;
+         }
+         delay(1000);
+       }
+       {
+         int i = 0;
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+           pixels.show();
+           delay(10);
+           i++;
+         }
+       }
+       delay(2000);
+       forward();
+       delay( 1000 );
+       Stop();
+
+     } else {
+       Serial.println("WHITE");
+
+       int i = 0;
+       pixels.clear();
+       while (i < 16) {
+         pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+         pixels.show();
+         delay(10);
+         i++;
+
+         digitalOutBinary(4);
+         val = analogRead(A0);
+         Serial.print("PSD");
+         Serial.println(val);
+
+         int PSDval = 0;
+         if (PSDval < 300) {
+           forward();
+         } else {
+           Stop();
+           delay(1000);
+           back();
+           delay(300);
+           Left() ;
+           delay(800);
+           Stop();
+           delay(100);
+           //back();
+           delay(400);
+           Stop();
+           delay(100);
+         }
+       }
+     }
+    } else {
+     if (Grc > Brc) {
+       Serial.println("GREEN");
+       Stop();
+       {
+         int i = 0;
+         pixels.clear();
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+           pixels.show();
+           delay(100);
+           i++;
+         }
+         delay(1000);
+       }
+       {
+         int i = 0;
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+           pixels.show();
+           delay(10);
+           i++;
+         }
+         delay(500);
+       }
+
+       {
+         int i = 0;
+         pixels.clear();
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+           pixels.show();
+           delay(100);
+           i++;
+         }
+         delay(1000);
+       }
+       {
+         int i = 0;
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+           pixels.show();
+           delay(10);
+           i++;
+         }
+         delay(500);
+       }
+       {
+         int i = 0;
+         pixels.clear();
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+           pixels.show();
+           delay(100);
+           i++;
+         }
+         delay(1000);
+       }
+       {
+         int i = 0;
+         while (i < 16) {
+           pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+           pixels.show();
+           delay(10);
+           i++;
+         }
+       }
+
+       delay(2000);
+       forward();
+       delay( 1000 );
+       Stop();
+
+
+     } else {
+       Serial.println("WHITE");
+       int i = 0;
+       pixels.clear();
+       while (i < 16) {
+         pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+         pixels.show();
+         delay(10);
+         i++;
+
+         int PSDval = 0;
+         digitalOutBinary(4);
+         PSDval = analogRead(A0);
+         Serial.print("PSD");
+         Serial.println(val);
+
+
+         if (PSDval < 300) {
+           forward();
+         } else {
+           Stop();
+           delay(1000);
+           back();
+           delay(300);
+           Left() ;
+           delay(800);
+           Stop();
+           delay(100);
+           //back();
+           delay(400);
+           Stop();
+           delay(100);
+         }
+       }
+     }
+    }
+  
+}
+*/
+
---
+++ RP2040-ToF/src/main.cpp
@@ -0,0 +1,139 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define addr 0x70//PCA9547Dのアドレス
+#define ADDRESS 0x52//ToFのアドレス
+
+#define WIRE1_SDA       6  // Use GP6 as I2C1 SDA
+#define WIRE1_SCL       7  // Use GP7 as I2C1 SCL
+arduino::MbedI2C Wire1(WIRE1_SDA, WIRE1_SCL);
+
+  // 閾値
+  int threshold_F = 100;
+  int threshold_R = 100;
+  int threshold_L = 100;
+
+int val_0;
+int val_1;
+int val_2;
+int val_3;
+/*
+val_0 = 前右
+val_1 = 横右
+val_2 = 横左
+val_3 = 前左
+*/
+
+int val_Front;
+
+uint16_t distance;
+uint16_t distance_tmp;
+uint8_t data_cnt;
+
+
+void ChangeChannel(char channel) //バスマルチのチャンネル変更
+{
+  Wire1.beginTransmission(addr);
+  Wire1.write(channel & 0x07 | 0x08);
+  Wire1.endTransmission();
+}
+
+void ToF()
+{
+    //Serial.print("distanceF1 = ");
+
+    Wire1.beginTransmission(ADDRESS);
+    Wire1.write(0xD3);
+    Wire1.endTransmission(false);
+    Wire1.requestFrom(ADDRESS, 2);
+    data_cnt = 0;
+    distance = 0;
+    distance_tmp = 0;
+    while (Wire1.available()) {
+      distance_tmp = Wire1.read();
+      distance = (distance << (data_cnt * 8)) | distance_tmp;
+      data_cnt++;
+    }
+
+    //Serial.print(distance);
+    //Serial.println(" mm");
+}
+
+
+void setup() {
+ Wire1.begin();
+ Serial.begin(9600);
+ Serial1.begin(9600);
+ Serial.println("MTOF171000C0_I2C");
+ pinMode(2, OUTPUT);
+}
+
+void loop() {
+  //ToFセンサ読み取り
+{  
+ ChangeChannel(0);
+ ToF();
+ val_0 = distance;
+ Serial.print("ToF_0 = ");
+ Serial.print(val_0);
+ Serial.println(" mm");
+ delay(50);
+}
+{  
+ ChangeChannel(1);
+ ToF();
+ val_1 = distance;
+ Serial.print("ToF_1 = ");
+ Serial.print(val_1);
+ Serial.println(" mm");
+  delay(50);
+}
+{  
+ ChangeChannel(2);
+ ToF();
+ val_2 = distance;
+ Serial.print("ToF_2 = ");
+ Serial.print(val_2);
+ Serial.println(" mm");
+  delay(50);
+}
+{  
+ ChangeChannel(3);
+ ToF();
+ val_3 = distance;
+ Serial.print("ToF_3 = ");
+ Serial.print(val_3);
+ Serial.println(" mm");
+  delay(50);
+}
+  //前センサ比較
+
+  int val_Front = min(val_0, val_3);
+  Serial.print("小さい方の値: ");
+  Serial.println(val_Front);
+
+
+  int result_0 = (val_Front < threshold_F) ? 1 : 0;
+  int result_1 = (val_1 < threshold_R) ? 1 : 0;
+  int result_2 = (val_2 < threshold_L) ? 1 : 0;
+
+  // 結果を出力(パソコン)
+  {
+  Serial.print("[");
+  Serial.print(result_0);
+  Serial.print(",");
+  Serial.print(result_1);
+  Serial.print(",");
+  Serial.print(result_2);
+  Serial.println("]");
+  }
+  // 結果を出力(メインマイコン)
+  {
+  Serial1.print(result_0);
+  Serial1.print(",");
+  Serial1.print(result_1);
+  Serial1.print(",");
+  Serial1.println(result_2);
+  }
+
+}
